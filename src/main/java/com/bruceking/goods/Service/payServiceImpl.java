8fc31916f1e2c898246827f17405b8c7310fda0b
JSYBruce <710627820@qq.com>
2020-06-16 22:01:44 +0800

    优化了商城和支付页面，添加了支付数据存入mysql的功能。

---
+++ src/main/java/com/bruceking/goods/Service/payServiceImpl.java
@@ -0,0 +1,19 @@
+package com.bruceking.goods.Service;
+
+import com.bruceking.goods.bean.Pay;
+
+import com.bruceking.goods.mapper.PayMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class payServiceImpl implements payService {
+
+    @Autowired
+    private PayMapper payMapper;
+
+    @Override
+    public void AddPay(Pay pay) {
+        payMapper.AddPay(pay);
+    }
+}
