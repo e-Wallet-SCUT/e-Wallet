a9850baa93971638e38c66c51689afd79f0b880a
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-06-19 22:03:36 +0800

    更新

--- src/main/java/com/bruceking/trans/TransApplication.java
+++ src/main/java/com/bruceking/trans/TransApplication.java
@@ -3,12 +3,15 @@ package com.bruceking.trans;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 @MapperScan("com.bruceking.trans.dao")
+
 public class TransApplication {
 
     public static void main(String[] args) {
