2aad0362cc11d370011854766f9b999cb5a7761f
袁登亚 <1405860103@qq.com>
2020-06-23 20:49:37 +0800

    trans06

--- src/main/java/com/bruceking/trans/dao/TransDao.java
+++ src/main/java/com/bruceking/trans/dao/TransDao.java
@@ -11,19 +11,19 @@ public interface TransDao {
     Account selectAccount(@Param("id") Integer id, @Param("type") Integer type);
 
     //插入转账记录
-    @Insert("insert into tranfer(transfer_pay_id,transfer_pay_type,transfer_target_type,transfer_target_id,transfer_amount,transfer_date,transfer_description)" +
-            " values(#{transfer_pay_id},#{transfer_pay_type},#{transfer_target_type},#{transfer_target_id},#{transfer_amount},NOW(),#{transfer_description})")
+    @Insert("insert into tranfer(transfer_pay_id,transfer_pay_account_id,transfer_target_account_id,transfer_target_id,transfer_amount,transfer_date,transfer_description)" +
+            " values(#{transfer_pay_id},#{transfer_pay_account_id},#{transfer_target_account_id},#{transfer_target_id},#{transfer_amount},NOW(),#{transfer_description})")
     void insertTransfer(Transfer transfer);
 
     //插入交易记录
     @Insert("insert into transaction(transaction_entity_long_id,transaction_entity_short_id,transaction_long_amount,transaction_short_amount,transaction_type,transaction_date,transaction_description)" +
-            " values(#{transfer_target_id},#{transfer_pay_id},#{transfer_amount},#{transfer_amount},\"transfer\",NOW(),#{transfer_description})")
+            " values(#{transfer_target_id},#{transfer_pay_id},#{transfer_amount},#{transfer_amount},'transfer',NOW(),#{transfer_description})")
     void insertTransaction(Transfer transfer);
 
     //更新账户余额
     @Update("update account set account_balance=account_balance-#{transfer_amount}" +
-            " where account_customer_id=#{transfer_pay_id} and account_type=#{transfer_pay_type};" +
-            " update account set account_balance=account_balance+#{transfer_amount}\n" +
-            " where account_customer_id=#{transfer_target_id} and account_type=#{transfer_target_type}")
+            " where account_customer_id=#{transfer_pay_account_id};"+
+            " update account set account_balance=account_balance+#{transfer_amount}" +
+            " where account_customer_id=#{transfer_target_account_id}")
     void updateAccount(Transfer transfer);
 }
--- src/main/java/com/bruceking/trans/entity/Transfer.java
+++ src/main/java/com/bruceking/trans/entity/Transfer.java
@@ -7,11 +7,29 @@ public class Transfer implements Serializable {
 
     private Integer transfer_pay_id;
     private Integer transfer_pay_type;
+    private Integer transfer_pay_account_id;
     private Integer transfer_target_id;
     private Integer transfer_target_type;
+    private Integer transfer_target_account_id;
     private Float transfer_amount;
     private Date transfer_date;
     private String transfer_description;
+
+    public Integer getTransfer_pay_account_id() {
+        return transfer_pay_account_id;
+    }
+
+    public void setTransfer_pay_account_id(Integer transfer_pay_account_id) {
+        this.transfer_pay_account_id = transfer_pay_account_id;
+    }
+
+    public Integer getTransfer_target_account_id() {
+        return transfer_target_account_id;
+    }
+
+    public void setTransfer_target_account_id(Integer transfer_target_account_id) {
+        this.transfer_target_account_id = transfer_target_account_id;
+    }
 
     public Integer getTransfer_pay_id() {
         return transfer_pay_id;
