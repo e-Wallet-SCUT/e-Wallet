2871e9f012196e19a1e6a42f5067162011fc0042
RevCiC <929275947@qq.com>
2020-07-23 15:11:01 +0800

    settlement v1.4.2 frontend test-1

--- src/main/java/com/bruceking/main/loginPage/SecurityConfiguration.java
+++ src/main/java/com/bruceking/main/loginPage/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/css/**","/fonts/**","/images/**","/js/**","/", "/assets/**","/getSessionID",
                     "/getUserInfo","/goodsIndex","/search/**","/auLogin","/setAuthCode","/login/**",
-                    "/SignTransaction","insertTransaction").permitAll()
+                    "/SignTransaction","insertTransaction","/settlement").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin(form -> form
---
+++ src/main/java/com/bruceking/main/settlement/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.bruceking.main.settlement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/settlement")
+    public String index(){
+        return "settlement";
+    }
+}
