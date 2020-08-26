2cce454b0966372a1598d7ac9cb0abda79b6d836
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-08-26 11:08:47 +0800

    bugfix

--- src/main/java/com/bruceking/main/settlement/controller/PageController.java
+++ src/main/java/com/bruceking/main/settlement/controller/PageController.java
@@ -1,12 +1,20 @@
 package com.bruceking.main.settlement.controller;
 
+import com.bruceking.main.userInfo.userInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private com.bruceking.main.userInfo.userInfoService userInfoService;
+
     @GetMapping("/settlement")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("username",userInfoService.getCurrentUser());
         return "settlement";
     }
     @GetMapping("/settlementInner")
